19150c108038f4fc9eef773652dee5435f27ddb1
speedt <13837186852@qq.com>
2017-10-12 10:22:26 +0800

    haha

--- src/login/run.bat
+++ src/login/run.bat
@@ -1,1 +1,1 @@
-del logs\*.log /q & npm install & npm install ..\lib & npm install ..\biz & node app.js
+set HTML_CDN=http://foreworld.net/& del logs\*.log /q & npm install & npm install ..\lib & npm install ..\biz &  node app.js
