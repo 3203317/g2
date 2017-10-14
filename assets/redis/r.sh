0ea72d652e20d4f1735e65afd27a53eed082a78b
speedt <13837186852@qq.com>
2017-10-14 09:53:50 +0800

    haha

--- assets/redis/r.sh
+++ assets/redis/r.sh
@@ -2,8 +2,8 @@
 
 echo ""
 echo "front_open.lua"
-/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 script load "$(cat /root/my/git/3203317/g2/assets/redis/front_open.lua)"
+/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 -p 12379 script load "$(cat /root/my/git/3203317/g2/assets/redis/front_open.lua)"
 
 echo ""
 echo "front_close.lua"
-/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 script load "$(cat /root/my/git/3203317/g2/assets/redis/front_close.lua)"
+/root/my/redis/redis-3.2.6/src/redis-cli -a 123456 -p 12379 script load "$(cat /root/my/git/3203317/g2/assets/redis/front_close.lua)"
