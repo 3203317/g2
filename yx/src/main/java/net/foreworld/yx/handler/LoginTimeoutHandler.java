0a3ad0abc2154130cb38e0f781e03f1cb8636a0e
unknown <3203317@qq.com>
2017-11-07 20:45:10 +0800

    haha

--- yx/src/main/java/net/foreworld/yx/handler/LoginTimeoutHandler.java
+++ yx/src/main/java/net/foreworld/yx/handler/LoginTimeoutHandler.java
@@ -1,20 +1,21 @@
 package net.foreworld.yx.handler;
 
-import javax.annotation.Resource;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.jms.core.JmsMessagingTemplate;
-import org.springframework.stereotype.Component;
-
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
+import java.net.InetSocketAddress;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.stereotype.Component;
+
 /**
- * 
+ *
  * @author huangxin <3203317@qq.com>
  *
  */
@@ -23,24 +24,21 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 @Sharable
 public class LoginTimeoutHandler extends ChannelInboundHandlerAdapter {
 
-	@Value("${server.id}")
-	private String server_id;
-
-	@Value("${queue.channel.close}")
-	private String queue_channel_close;
-
 	@Resource(name = "jmsMessagingTemplate")
 	private JmsMessagingTemplate jmsMessagingTemplate;
 
-	private static final Logger logger = LoggerFactory.getLogger(LoginTimeoutHandler.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(LoginTimeoutHandler.class);
 
 	@Override
 	public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
-		removeChannel(ctx.channel().id().asLongText());
+		InetSocketAddress addr = (InetSocketAddress) ctx.channel()
+				.remoteAddress();
+		removeChannel(addr.getAddress().getHostAddress());
 		super.channelUnregistered(ctx);
 	}
 
-	private void removeChannel(String channel_id) {
-		logger.info("channel close: {}:{}", server_id, channel_id);
+	private void removeChannel(String ip) {
+		logger.info("channel close: {}", ip);
 	}
 }
