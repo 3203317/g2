adf4fea16307a0947398bd5166a5af1cb755eb38
huangxin <3203317@qq.com>
2017-12-04 18:04:06 +0800

    haha

--- yx/src/main/java/net/foreworld/yx/handler/BackTimeHandler.java
+++ yx/src/main/java/net/foreworld/yx/handler/BackTimeHandler.java
@@ -1,34 +1,31 @@
 package net.foreworld.yx.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import net.foreworld.yx.model.BackModel;
 import net.foreworld.yx.util.SendUtil;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
 /**
- *
- * @author huangxin
+ * 
+ * @author huangxin <3203317@qq.com>
  *
  */
 @Component
 @Sharable
 public class BackTimeHandler extends SimpleChannelInboundHandler<BackModel> {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(BackTimeHandler.class);
+	private static final Logger logger = LoggerFactory.getLogger(BackTimeHandler.class);
 
 	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, BackModel msg)
-			throws Exception {
-		logger.info("{}:{}", msg.getMethod(), msg.getReceiver());
+	protected void channelRead0(ChannelHandlerContext ctx, BackModel msg) throws Exception {
+		logger.info("{}:{}", msg.getReceiver(), msg.getMethod());
 
 		SendUtil.backSend(msg.getReceiver(), msg.getData());
-		ctx.flush();
 	}
 
 }
