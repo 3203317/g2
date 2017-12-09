4a392f2ffefdbff7ac707d3e6581c6a36d09c473
huangxin <3203317@qq.com>
2017-12-09 10:09:41 +0800

    haha

--- yx/src/main/java/net/foreworld/yx/client/ZkClient.java
+++ yx/src/main/java/net/foreworld/yx/client/ZkClient.java
@@ -1,11 +1,11 @@
 package net.foreworld.yx.client;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
-import net.foreworld.util.Client;
-
+import org.apache.commons.codec.CharEncoding;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
@@ -19,6 +19,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+
+import net.foreworld.util.Client;
+import net.foreworld.yx.util.Constants;
+import net.foreworld.yx.util.MethodUtil;
 
 /**
  *
@@ -45,8 +49,7 @@ public class ZkClient extends Client implements Watcher {
 	@Value("${server.id}")
 	private String server_id;
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(ZkClient.class);
+	private static final Logger logger = LoggerFactory.getLogger(ZkClient.class);
 
 	@Override
 	public void shutdown() {
@@ -72,8 +75,9 @@ public class ZkClient extends Client implements Watcher {
 	 *
 	 * @throws InterruptedException
 	 * @throws KeeperException
+	 * @throws UnsupportedEncodingException
 	 */
-	private void register() throws KeeperException, InterruptedException {
+	private void register() throws KeeperException, InterruptedException, UnsupportedEncodingException {
 		registerServer();
 		listenerService();
 	}
@@ -84,8 +88,7 @@ public class ZkClient extends Client implements Watcher {
 	 * @throws InterruptedException
 	 */
 	private void registerServer() throws KeeperException, InterruptedException {
-		zk.create(zk_rootPath + "/front/" + server_id, "".getBytes(),
-				Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+		zk.create(zk_rootPath + "/front/" + server_id, "".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
 	}
 
 	private Watcher watcher;
@@ -97,15 +100,37 @@ public class ZkClient extends Client implements Watcher {
 	 *
 	 * @throws KeeperException
 	 * @throws InterruptedException
+	 * @throws UnsupportedEncodingException
 	 */
-	private void listenerService() throws KeeperException, InterruptedException {
-		List<String> list = zk.getChildren(zk_rootPath + "/service", watcher,
-				stat);
+	private void listenerService() throws KeeperException, InterruptedException, UnsupportedEncodingException {
+		List<String> list = zk.getChildren(zk_rootPath + "/service", watcher, stat);
 
 		logger.info("service count: {}", stat.getNumChildren());
 
 		for (int i = 0; i < list.size(); i++) {
-			logger.info("service name: {}", list.get(i));
+			String serv_name = list.get(i);
+			String serv_val = new String(zk.getData(zk_rootPath + "/service/" + serv_name, false, null),
+					CharEncoding.UTF_8);
+			logger.info("service name: {}, value: {}", serv_name, serv_val);
+
+			String[] _keys = serv_name.split(":");
+
+			switch (_keys.length) {
+			case 2:
+				MethodUtil.getDefault().put(_keys[0], Constants.MQ);
+				break;
+			case 3:
+				MethodUtil.getDefault().put(_keys[1] + ":" + _keys[2], Constants.MQ);
+				break;
+			case 4:
+				if (server_id.equals(_keys[3]))
+					MethodUtil.getDefault().put(_keys[1], serv_val);
+				break;
+			case 5:
+				if (server_id.equals(_keys[4]))
+					MethodUtil.getDefault().put(_keys[2] + ":" + _keys[3], serv_val);
+				break;
+			}
 		}
 	}
 
@@ -128,19 +153,24 @@ public class ZkClient extends Client implements Watcher {
 				public void process(WatchedEvent event) {
 					try {
 						listenerService();
-					} catch (KeeperException | InterruptedException e) {
+					} catch (KeeperException | InterruptedException | UnsupportedEncodingException e) {
 						logger.error("", e);
 					}
 				}
 			};
-
 	}
 
 	@Override
-	public void start() throws IOException, InterruptedException,
-			KeeperException {
+	public void start() throws IOException, InterruptedException, KeeperException {
 		init();
 		register();
+	}
+
+	public static void main(String[] args) {
+		System.err.println("方法:后置机".split(":").length);
+		System.err.println(":方法:后置机".split(":").length);
+		System.err.println(":方法:后置机:前置机".split(":").length);
+		System.err.println("::方法:后置机:前置机".split(":").length);
 	}
 
 }
